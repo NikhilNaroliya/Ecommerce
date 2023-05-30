103ae6f919babd6c3799470e76cf858bdc0f7adf
nikhil naroliya <nikhilnaroliya7exams@gmail.com>
2023-05-30 14:57:35 +0530

    added category section designs

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,25 @@
-import "./Category.scss";
-const Category = () => {
-    return <div>Category</div>;
-};
+import './Category.scss'
 
-export default Category;
+import cat1 from '../../assets/category/cat-1.jpg'
+
+const Category =()=>{
+    return (
+        <div className="category-main-content " >
+            <div className="categories">
+                <div className="category">
+                    <img className="cat-img" src={cat1} alt="" />
+                </div>
+                <div className="category">
+                    <img className="cat-img" src={cat1} alt="" />
+                </div>
+                <div className="category">
+                    <img className="cat-img" src={cat1} alt="" />
+                </div>
+                <div className="category">
+                    <img className="cat-img" src={cat1} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Category
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,10 +2,16 @@ import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Newsletter from "../Footer/Newsletter/Newsletter";
 import Footer from "../Footer/Footer";
+import Category from "../Category/Category";
 const Home = () => {
-    return <div className="Home">
+    return <div >
         <Banner/>
-    
+        <div className="main-content">
+            <div className="layout">
+                
+                  <Category/>
+            </div>
+        </div>
     
     </div>;
 };
